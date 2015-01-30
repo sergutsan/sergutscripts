784d1da949729c6b61abf3116d947723748b7d92
Sergio Gutierrez-Santos (Sergut) <sergut@gmail.com>
2015-01-30 17:46:52 +0000

    Now the name of the person is shown.

--- mostrarCarismaUmbriano.sh
+++ mostrarCarismaUmbriano.sh
@@ -8,12 +8,17 @@ BASE_URL="http://www.comunidadumbria.com/comunidad/companeros/pestana/carisma/"$
 TMP_WEB_FILE=/tmp/$1.tmp.html
 TMP_TXT_FILE=/tmp/$1.tmp.txt
 wget $BASE_URL -O $TMP_WEB_FILE
-echo "Charisma points: "
-cat $TMP_WEB_FILE \
-  | grep 'div class="opinion' -A4 \
-  | grep src |awk 'BEGIN{FS="alt=\""}{print($2)}' \
-  | awk 'BEGIN{FS="\""}{print($1)}' \
-  | sort \
+USER=`cat $TMP_WEB_FILE         \
+       | grep '<title>'         \
+       | awk 'BEGIN{FS="sobre "}{print($2)}'   \
+       | awk 'BEGIN{FS="</title>"}{print($1)}'`
+echo "Charisma points for '$USER': "
+cat $TMP_WEB_FILE                      \
+  | grep 'div class="opinion' -A4      \
+  | grep src                           \
+  |awk 'BEGIN{FS="alt=\""}{print($2)}' \
+  | awk 'BEGIN{FS="\""}{print($1)}'    \
+  | sort    \
   | uniq -c \
   | sort -r \
   | awk '    {                                            \
@@ -29,4 +34,5 @@ cat $TMP_WEB_FILE \
 # Indice de endogamia = (suma de puntuaciones de carisma / cuenta de opinadores únicos) - 1, normalizado de 0 a 1 (N/N+1)
 #       Persona con N puntos procedentes de N personas diferentes, endogamia = 0
 #       Persona con N puntos procedentes todos de la misma persona, endogamia -> 1 cuando N -> inf
-
+# 
+# Deberia tal vez sum(N^p)/count(N) donde p es mayor que uno para penalizar la concentración?
